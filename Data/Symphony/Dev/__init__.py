d00bbcf30dcb9a54a38e826d864313dd5bc7e0a7
symphony_localadmin_user <symphonylocal@EU1APP57P.PVHCORP.COM>
2025-03-07 11:33:35 +0100

    pvh

--- Data/Symphony/Dev/__init__.py
+++ Data/Symphony/Dev/__init__.py
@@ -3,14 +3,5 @@
 # Warning: Any variable with conflicting names will be overwritten with 
 #          values as they are imported through each file.
 
-from .SA_Roles import *
-from .SalesDoc import *
-from .UserProfile import *
-from .MIRO import *
-from .DTA_TS4 import *
-from .Response import *
+
 from .PVH import *
-from .Fiori import *
-from .Copilot import *
-from .Idoc import *
-from .Salesforce import *
